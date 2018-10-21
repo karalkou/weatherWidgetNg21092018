67f0bdb29fbcf16daa20bd8c3ce7d35413ac005f
Yury Karalkou <Yury_Karalkou@epam.com>
2018-10-21 20:42:29 +0300

    Add styles and images for homework

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'weatherVidgetNg21092018';
+  public title = 'weatherVidgetNg21092018';
 }
