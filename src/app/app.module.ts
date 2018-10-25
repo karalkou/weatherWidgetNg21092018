9e7868fd3e948149b9dbecd9eafaf7e1344f6acf
Yury Karalkou <Yury_Karalkou@epam.com>
2018-10-25 20:59:24 +0300

    Move 'social-card' and 'weather-card' to components folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { WeatherCardComponent } from './weather-card/weather-card.component';
-import { SocialCardComponent } from './social-card/social-card.component';
+import { WeatherCardComponent } from './components/weather-card/weather-card.component';
+import { SocialCardComponent } from './components/social-card/social-card.component';
 
 @NgModule({
   declarations: [
